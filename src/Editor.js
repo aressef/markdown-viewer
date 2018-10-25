b0a03b219e1496bf0a9823bf4e3506624217b7c9
Ryan Fuhrman <rsfuhrman10@gmail.com>
2018-10-25 12:08:57 -0400

    I didnt change anything in this file

--- src/Editor.js
+++ src/Editor.js
@@ -1,17 +1,12 @@
 import React from "react";
 
 class Editor extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <div className="editor-div">
         <h2>Editor</h2>
         <textarea
           id="editor"
-          ref="editor"
           onChange={this.props.handleChange}
           value={this.props.input}
         />
