234df8c9736d8c9cea824f04648755233b2e7e28
Ryan Fuhrman <rsfuhrman10@gmail.com>
2018-10-25 12:07:03 -0400

    Set marked option for breaks to true to render carriage returns as <br> (line breaks)

--- src/Preview.js
+++ src/Preview.js
@@ -1,14 +1,16 @@
 import React from "react";
-import marked from "marked";
+// import marked from "marked";
 
 class Preview extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   componentDidUpdate(prevProps) {
+    const myMarked = require("marked");
+    // set breaks to true so carriage returns are rendered as <br> (line breaks)
+    myMarked.setOptions({
+      breaks: true
+    });
+
     if (this.props.output !== prevProps.output) {
-      this.refs.preview.innerHTML = marked(this.props.output);
+      this.refs.preview.innerHTML = myMarked(this.props.output);
     }
   }
 
